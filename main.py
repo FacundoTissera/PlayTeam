9b4bb1640de36b7047a73d26c7a8bde074a2d163
Facundo <facundotisserasorribas@gmail.com>
2022-11-29 06:20:44 -0300

    modificacion de menu para juego piedra papel tijera

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def menu():
+    
     global opcion, salir
     print('¿Que juego quieres jugar?');
     print('1. BlackJack');
@@ -7,21 +8,27 @@ def menu():
     print('4. Piedra, Papel o Tijera');
     print('5. Salir');
     opcion = input('Elige una opcion: ');
+    
     if opcion == '1':
         from blackJack import comenzarJuego;
         comenzarJuego();
+    
     elif opcion == '2':
         from PasaPalabra import entrada;
         entrada()
+    
     elif opcion == '3':
         from preguntados import jugar1;
         jugar1();
-        print('Preguntados');
+        
     elif opcion == '4':
-        from PPT_agos import jugada;
+        from piedrapapeltijera import piedraPapelTijera;
+        piedraPapelTijera();
+    
     elif opcion == '5':
         print('Hasta luego!');
         exit();
+    
     salir = opcion
 
 
